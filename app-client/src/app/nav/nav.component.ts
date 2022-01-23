4c15597733918611cebcd04893df8b7aced9a021
Jenith2k15 <you@example.com>
2022-01-23 19:35:56 +0530

    more features added

--- app-client/src/app/nav/nav.component.ts
+++ app-client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -11,7 +13,8 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model:any = {};
 
-  constructor(public accountService:AccountService) { }
+  constructor(public accountService:AccountService, private router:Router, 
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
     console.log("step 4 from nav lifecycle");
@@ -21,15 +24,18 @@ export class NavComponent implements OnInit {
   login() {
     console.log("step 5 from nav");
     this.accountService.login(this.model).subscribe(response => {
+      this.router.navigateByUrl('/members');
       console.log("step 10 successfull logined");
       console.log(response);
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
   logout() {
     console.log("step 11 logout from nav");
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
