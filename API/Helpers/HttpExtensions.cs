421ab5c450c903c3c3c1abfc1d232fd4bd51aa34
Jenith2k15 <you@example.com>
2022-04-03 18:34:53 +0530

    till pagination completed

--- API/Helpers/HttpExtensions.cs
+++ API/Helpers/HttpExtensions.cs
@@ -13,7 +13,13 @@ namespace API.Helpers
         int totalItems, int totalPages)
         {
            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
-           response.Headers.Add("Paginaion", JsonSerializer.Serialize(paginationHeader));
+
+           var options = new JsonSerializerOptions
+           {
+              PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+           };
+
+           response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader, options));
            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
         }
     }
